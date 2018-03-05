2442556dc8109302fe2ccd2b8267c158fad37483
vikky1993 <wickee0810@gmail.com>
2018-03-05 08:13:23 +0530

    Remove

--- trydjango/src/restaurants/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class RestaurantCreateForm(forms.Form):
-	name		= forms.CharField()
-	location	= forms.CharField(required=False)
-	category 	= forms.CharField(required=False)
